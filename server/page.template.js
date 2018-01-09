74f0af4b4378032b99ad78c1f8dfd41d54580b35
Will Clark <willdavidc@gmail.com>
2018-01-09 00:32:47 -0500

    Test layout on mobile

--- server/page.template.js
+++ server/page.template.js
@@ -2,7 +2,7 @@
 module.exports = (vitreum) => {
     return `<html>
 	<head>
-        <meta name="viewport" content="width=1000">
+        <meta name="viewport" content="width=750, user-scalable=no">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
 		${vitreum.head}
