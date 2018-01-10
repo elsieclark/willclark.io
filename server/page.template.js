69bfb805189f404ff6c4be01139ca8c5afcd7426
Will Clark <willdavidc@gmail.com>
2018-01-10 09:31:02 -0500

    Widen page size on mobile

--- server/page.template.js
+++ server/page.template.js
@@ -2,7 +2,7 @@
 module.exports = (vitreum) => {
     return `<html>
     <head>
-        <meta name="viewport" content="width=750, user-scalable=no">
+        <meta name="viewport" content="width=900, user-scalable=no">
         <!--<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />-->
         <script src="https://use.fontawesome.com/af4e1da625.js"></script>
         ${vitreum.head}
